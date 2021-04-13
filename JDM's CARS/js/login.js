d5cba5bc3bba873dbb4a886708004ee3343c4459
JacqueEvander <55414936+JacqueEvander@users.noreply.github.com>
2021-04-13 02:43:25 -0400

    dike push?

--- JDM's CARS/js/login.js
+++ JDM's CARS/js/login.js
@@ -34,4 +34,24 @@ const users = [
 	  }
 	}
   );
+
+  
+
+
+
+  // Register
+  const buttonRegister = document.getElementById("buttonRegister");
+  buttonRegister.addEventListener("click", e => {
+	  e.preventDefault();
+	  const inputUserNameRegister = document.getElementById("inputUserNameRegister");
+	  const inputPasswordRegister = document.getElementById("inputPasswordRegister");
+	console.log(inputUserNameRegister)
+	  console.log(inputPasswordRegister)
+	  const newUser=[{
+		username: inputUserNameRegister,
+		password: inputPasswordRegister
+	  }]
+
+	  users.push(newUser);
+  })
   
--- JDM's CARS/js/register.js
+++ JDM's CARS/js/register.js
@@ -1,18 +1,30 @@
 
 
-
+const users = [
+	{
+	  username: "eddy",
+	  email:"eddy@jdm.com",
+	  password: "1234"
+	},
+	{
+	  username: "Valentin Sanchez",
+	  email: "valentin.sanchez13@gmail.com",
+	  password: "bestprof"
+	},
+  ];
 
   // Register
   const buttonRegister = document.getElementById("buttonRegister");
   buttonRegister.addEventListener("click", e => {
 	  e.preventDefault();
-	  const inputUserNameRegister = document.getElementById("inputUserNameRegister");
-	  const inputPasswordRegister = document.getElementById("inputPasswordRegister");
-	
-	  const newUser=[{
+	  const inputUserNameRegister = document.getElementById("inputUserNameRegister").value;
+	  const inputPasswordRegister = document.getElementById("inputPasswordRegister").value;
+	  const inputEmailRegister = document.getElementById("inputEmailRegister").value;
+	  
+	  const newUser={
 		username: inputUserNameRegister,
+		email:inputEmailRegister,
 		password: inputPasswordRegister
-	  }]
-
+	  };
 	  users.push(newUser);
   })

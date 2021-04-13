8d4605e79125fa9e1b4ae208c5053374575436f6
JacqueEvander <55414936+JacqueEvander@users.noreply.github.com>
2021-04-13 15:39:28 -0400

    los santos custom 3 cars check it

--- JDM's CARS/js/paint.js
+++ JDM's CARS/js/paint.js
@@ -11,14 +11,14 @@ function changeBackground() {
 // // Random Los Santos Fotix
 function randomImgPaint() {
   const paths = [
-    `<img src="resources/img/paint/torqueGT.png">`,
+    `<img src="/JDM's CARS/IMG/santosgtr.png">`,
     `<img src="/JDM's CARS/IMG/santossupra.png">`,
-    `<img src="resources/img/paint/nissanGTR.png">`,
+    `<img src="/JDM's CARS/IMG/santoschaser.png">`,
+    
   ];
   const randomNumber = Math.floor(Math.random() * 3);
   // let img = document.createElement("img");
   // img.setAttribute("src", paths[0])
   const divImage = document.getElementById("divImage");
-  // divImage.innerHTML = paths[randomNumber];
-  divImage.innerHTML = `<img src="/JDM's CARS/IMG/suprasanto.png"></img>`
+  divImage.innerHTML = paths[randomNumber];
 }
